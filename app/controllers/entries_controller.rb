2bd45a9045543bca7fda048ebe53cbe6d8b6cea0
Dana <doneil1580@gmail.com>
2018-10-15 14:40:52 -0400

    added the tables for users and entries as well as generated the resource for entry and added the form partial

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,46 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:edit, :show, :update]
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to entries_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :content)
+  end
+
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
 end

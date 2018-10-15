d557ec9dfe750e07531fc7ecde794d381cb49dd5
Dana <doneil1580@gmail.com>
2018-10-15 14:02:31 -0400

    added the custom routes for signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :entries
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy'
+  end 
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
